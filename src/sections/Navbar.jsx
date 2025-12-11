f340fed4c3679aa64f3fc11b131fbf51a6566252
AKOI2000 <alausayinka@gmail.com>
2025-12-11 22:53:49 +0100

    Navbar responsiveness

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="navbar nav-container">
       <div className="navbar_logo-box">
         <img src="/Logo.png" alt="" />
       </div>
-      
-      <nav className="navbar_nav">
-        <a href="" className="navbar_nav-links">Home</a>
-        <a href="#" className="navbar_nav-links">About</a>
-        <a href="" className="navbar_nav-links">Leadership</a>
-        <a href="" className="navbar_nav-links">Services</a>
-        <a href="" className="navbar_nav-links">Contact</a>
+
+      <nav className={`navbar_nav ${isActive ? "active" : ""}`}>
+        <a href="" className="navbar_nav-links">
+          Home
+        </a>
+        <a href="#" className="navbar_nav-links">
+          About
+        </a>
+        <a href="#" className="navbar_nav-links">
+          Leadership
+        </a>
+        <a href="#" className="navbar_nav-links">
+          Services
+        </a>
+        <a href="#" className="navbar_nav-links">
+          Contact
+        </a>
       </nav>
 
       <div className="navbar_cta">
-        <a href="" className="btn btn-main">Book a consultation</a>
+        <a href="" className="btn btn-main">
+          Book a consultation
+        </a>
+        <div
+          className={`nav-hamburger ${isActive ? "active" : ""}`}
+          onClick={() => setIsActive((active) => !active)}
+        >
+          <span className="span-1"></span>
+          <span className="span-2"></span>
+          <span className="span-3"></span>
+        </div>
       </div>
     </div>
   );
